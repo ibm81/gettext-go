e9e00b46200a5fea974e44720bfd2fc80cf0dc75
chai2010 <chaishushan@gmail.com>
2020-04-13 08:25:31 +0800

    zz

--- cmd/xgettext-go/main.go
+++ cmd/xgettext-go/main.go
@@ -8,26 +8,52 @@ package main
 import (
 	"fmt"
 	"go/ast"
+	"go/importer"
 	"go/parser"
 	"go/token"
+	"go/types"
 	"log"
 )
 
 func main() {
 	fset := token.NewFileSet()
-	pkgs, firstErr := parser.ParseDir(fset, "../../examples/hi", nil, 0)
+	astPackages, firstErr := parser.ParseDir(fset, "../../examples/hi", nil, 0)
 	if firstErr != nil {
 		log.Fatal(firstErr)
 	}
 
-	for _, pkg := range pkgs {
+	var astFiles []*ast.File
+	for _, pkg := range astPackages {
+		for _, f := range pkg.Files {
+			astFiles = append(astFiles, f)
+		}
+	}
+
+	// https://github.com/golang/go/issues/26504
+	typesConfig := &types.Config{
+		Importer:    importer.For("source", nil),
+		FakeImportC: true,
+	}
+	typesInfo := &types.Info{
+		Types:      make(map[ast.Expr]types.TypeAndValue),
+		Defs:       make(map[*ast.Ident]types.Object),
+		Uses:       make(map[*ast.Ident]types.Object),
+		Selections: make(map[*ast.SelectorExpr]*types.Selection),
+	}
+
+	typesPackage, err := typesConfig.Check("", fset, astFiles, typesInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, pkg := range astPackages {
 		for _, f := range pkg.Files {
 			ast.Inspect(f, func(n ast.Node) bool {
 				switch x := n.(type) {
 				case *ast.CallExpr:
 					switch sel := x.Fun.(type) {
 					case *ast.SelectorExpr:
-						if sel.X.(*ast.Ident).Name == "gettext" && sel.Sel.Name == "Gettext" {
+						if isGettextPackage(fset, typesPackage, sel.X) {
 							fmt.Println("msgstr:", evalStringValue(x.Args[0]))
 						}
 					}
@@ -36,6 +62,22 @@ func main() {
 			})
 		}
 	}
+}
+
+func isGettextPackage(fset *token.FileSet, pkg *types.Package, node ast.Node) bool {
+	inner := pkg.Scope().Innermost(node.Pos())
+	if ident, ok := node.(*ast.Ident); ok {
+		if _, obj := inner.LookupParent(ident.Name, node.Pos()); obj != nil {
+			if pkgName, ok := obj.(*types.PkgName); ok {
+				if pkg := pkgName.Imported(); pkg != nil {
+					if pkg.Path() == "github.com/chai2010/gettext-go" {
+						return true
+					}
+				}
+			}
+		}
+	}
+	return false
 }
 
 func evalStringValue(val interface{}) string {
